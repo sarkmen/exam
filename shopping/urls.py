bdcbf64bc8c13b0675b394b6114360f2ad03e4d9
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-06-08 14:59:01 +0900

    Category new,edit,detail 뷰 생성 및 템플릿 추가

--- shopping/urls.py
+++ shopping/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^category/new/$', views.category_new, name='category_new'),
+    url(r'^category/(?P<pk>\d+)/edit/$', views.category_edit, name='category_edit'),
+    url(r'^category/(?P<pk>\d+)/$', views.category_detail, name='category_detail'),
 ]
--- shopping/views.py
+++ shopping/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Category
+from .forms import CategoryForm
+
 
 def index(request):
     return render(request, 'shopping/index.html')
+
+
+def category_new(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST, request.FILES)
+        if form.is_valid():
+            category = form.save()
+            return redirect('shopping:category_detail', category.pk)
+    else:
+        form = CategoryForm()
+    return render(request, 'shopping/category_form.html', {
+        'form' : form
+        })
+
+
+def category_edit(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == "POST":
+        form = CategoryForm(request.POST, request.FILES, instance=category)
+        if form.is_valid():
+            category = form.save()
+            return redirect('shopping:category_detail', category.pk)
+    else:
+        form = CategoryForm()
+    return render(request, 'shopping/category_form.html', {
+        'form' : form
+        })
+
+
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'shopping/category_detail.html', {
+        'category' : category
+        })
+
+
+
