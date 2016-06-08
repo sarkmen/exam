3afbdd775bec45f47ea2b8adbbcf3a8aa317baf3
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-06-08 15:54:50 +0900

    login,logout 뷰 적용

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login, logout
+
 from . import views
 
 urlpatterns = [
+    url(r'^login/$', login),
+    url(r'^logout/$', logout),
 
 ]
