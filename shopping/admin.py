f81871d89081b32c43b326ec4db27da4d2695fdf
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-06-08 14:42:40 +0900

    admin.py에 모델 3개 등록

--- shopping/admin.py
+++ shopping/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Shop, Review
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Shop)
+admin.site.register(Review)
